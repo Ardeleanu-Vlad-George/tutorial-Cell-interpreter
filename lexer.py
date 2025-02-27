bf6e943cdca3f1d2996643ec08a7ee1d83e28924
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-02-27 10:14:11 +0200

    Reordered files

--- lexer.py
+++ lexer.py
@@ -1,38 +1,79 @@
 #!/usr/bin/python3
+import re
 
-source = "x=10;\ny=x+101;\nprint(\"This is the value:\"+y);\n"
+# the 'stream' class that will be used for progressing through the 
+# 'source' data, the data that represents the text of the source file
 
-isalpha = lambda chr: ('a' <= chr and chr <= 'z') or ('A' <= chr and chr <= 'Z')
+# A class serving a similar purpose to the 'std::stringstream' of C++
+class stream:
+    def __init__(self, source):
+        self.iterator = iter(source)
+        self.try_next()
 
-isdigit = lambda chr: '0' <= chr and chr <= '9'
+    # 'try' moving forward, update 'next' accordingly
+    def try_next(self):
+        try:
+            self.next = next(self.iterator)
+        except StopIteration:
+            self.next = None
 
-def literal_num(src, beg):
-    result = ''
-    while isdigit(src[beg]):
-        result+=src[beg]
-        beg+=1
+    # 'cut' the next from the data, try moving forward
+    def cut_next(self):
+        result = self.next
+        self.try_next()
+        return result
+
+isdigit = lambda ch: '0' <= ch and ch <= '9'
+
+isletter= lambda ch: ('a' <= ch and ch <= 'z') or ('A' <= ch and ch <= 'Z') 
+
+def get_string(quote_type, data_stream):
+    result = ""
+    while data_stream.next != quote_type:
+        ch = data_stream.cut_next()
+        if ch is None:
+            raise Exception("No equivalent quotiation mark was found")
+        result += ch
+    data_stream.cut_next()
     return result
 
-def lex(src):
-    for idx in range(len(src)):
-        if src[idx] in " \n":
-            continue
-        elif src[idx] in "[]{}(),;:=":
-            yield (src[idx], "")
-        elif isalpha(src[idx]):
-            yield ("alpha", src[idx])
-        elif isdigit(src[idx]):
-            value = literal_num(src, idx)
-            print(idx)
-            idx += len(value)
-            print(idx)
-            yield ("number", value)
+def get_sequen(seq_beg, data_stream, seq_allowed):
+    result = seq_beg
+    nxt = data_stream.next
+    while nxt is not None and re.match(seq_allowed, nxt):
+        result += data_stream.cut_next()
+        # result += data_stream.try_next() 
+        # the above lien was causing an error, you thought that the implementation
+        # of 'try_next' was to blame, except it wasn't for 'try_next' shouldn't 
+        # return anything. Once you called the proper function, 'cut_next' 
+        # it worked properly
+        nxt = data_stream.next 
+    return result
+
+def lex(source_code):
+    strm = stream(source_code)
+    while strm.next is not None:
+        ch = strm.cut_next()
+        if ch in " \n": # Ignore the white cases
+            pass
+        elif ch in "(){},;:=": # Special tokens
+            yield (ch, "")
+        elif ch in "+*-/":
+            yield ("op", ch)
+        elif ch in "'\"":
+            yield ("sr", get_string(ch, strm))
+        elif isdigit(ch):
+            yield ("nr", get_sequen(ch, strm, "[.0-9]"))
+        elif re.match("[_a-zA-Z]", ch):
+            yield ("id", get_sequen(ch, strm, "[_a-zA-Z0-9]"))
+        elif ch == "\t":
+            raise Exception("TAB is an illegal character inside Cell")
         else:
-            yield ("unknown", src[idx])
+            raise Exception("Unidentified char detected")
     else:
-        yield ("eos", "") # eos - End Of Source
+        yield ("eos", "")
 
-
-print("Source code after being tokenized:")
-print(tuple(lex(source)))
-
+    #if __name__ == "main":
+if __name__ == "__main__":
+    source = "x=10;\ny=x+101;\nprint(\"This is the result:\" + y);"
+    print(tuple(lex(source)))
