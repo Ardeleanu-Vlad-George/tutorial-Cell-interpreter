bbd39c2f6a6c578e4b13e84f93392d3ec7529f66
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-03-01 19:25:14 +0200

    Found the most satisfying solution for the 'list of tokens'

--- lib/spacious_aligned_prints.py
+++ lib/spacious_aligned_prints.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+import cmd 
 
 if __name__ == '__main__':
-    toks = (('id', 'x'), ('=', ''), ('nr', '-230'), (';', ''))
-    toks = 
-    AST = 
+    toks = (('id', 'x'), ('=', ''), ('nr', '10'), (';', ''), ('id', 'y'), ('=', ''), ('id', 'x'), ('op', '+'), ('nr', '101'), (';', ''), ('id', 'MSG'), ('=', ''), ('sr', 'This is the result:'), (';', ''), ('id', 'print'), ('(', ''), ('id', 'MSG'), (',', ''), ('id', 'y'), (')', ''), (';', ''))
+    str_toks = tuple(str(toks[_]) for _ in range(len(toks)))
+    # print(str_toks)
+    # cmd.Cmd.columnize(list(str_toks))
+    c = cmd.Cmd()
+    c.columnize(str_toks)
     print('Running in main')
+
 (('id', 'x'), ('=', ''), ('nr', '10'), (';', ''), ('id', 'y'), ('=', ''), ('id', 'x'), ('op', '+'), ('nr', '101'), (';', ''), ('id', 'MSG'), ('=', ''), ('sr', 'This is the result:'), (';', ''), ('id', 'print'), ('(', ''), ('id', 'MSG'), (',', ''), ('id', 'y'), (')', ''), (';', ''))
 (('asgn', ('id', 'x'), ('nr', '10')), ('asgn', ('id', 'y'), ('comp', '+', ('id', 'x'), ('nr', '101'))), ('call', ('id', 'print'), [('comp', '+', ('sr', 'This is the result:'), ('id', 'y'))]))
