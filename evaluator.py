610a05d9b47b24280d8e393a51b91e01b17e8a16
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-03-05 09:10:59 +0200

    Evaluator mostly done, time to test it

--- evaluator.py
+++ evaluator.py
@@ -20,8 +20,20 @@ def eval_exp(exp,env):
         ret = env.search(name)
         return ret
     elif act == 'asgn':
-    # to continue
+        var_name = exp[1][1]
+        val = eval_exp(exp[2], env)
+        env.items[var_name] = val
+        return val
+    elif act == 'call'
+        return call(exp, env)
+    # to complete with the part where you define a function
 
+
+def call(exp, env):
+    fn_name = exp[1]
+    fn_args = list(eval_exp(_, env) for _ in exp[2])
+    # keep it simple
+    print(fn_name, fn_args)
 
 def comp(exp, env):
     p1 = eval_exp(exp[2], env)
